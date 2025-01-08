5be9c792e278eb79b2c049d63d44802266827a44
Ashish Prajapati <108178021+ashCode98@users.noreply.github.com>
2025-01-08 18:02:02 +0530

    fix: update fetch URL to use hardcoded endpoint and log shortened URL

--- client/app.js
+++ client/app.js
@@ -2,12 +2,20 @@ const originalURL = document.getElementById('url');
 const btn = document.querySelector('button');
 const content = document.getElementById('main-content');
 
+async function temp_p() {
+    const temp_data = await fetch('https://url-shortening-service-3bfx.onrender.com/B9E5u/')
+    const data = await temp_data.json();
+    console.log(data);
+}
+
+// temp_p();
+
 btn.addEventListener('click', async (e) => {
     e.preventDefault();
     const url = originalURL.value;
 
     try {
-        const response = await fetch(`${process.env.BASE_URL}/shorten`, {
+        const response = await fetch(`https://url-shortening-service-3bfx.onrender.com/shorten`, {
             method: 'post',
             headers: {
                 'Content-Type': 'application/json'
@@ -17,18 +25,14 @@ btn.addEventListener('click', async (e) => {
 
         const data = await response.json();
         const shortenedUrl = data.shortenedUrl;
+        console.log(shortenedUrl);
 
-        const div = document.createElement('div');
-        div.className = 'shortened-url';
-        div.innerHTML = `Here is your shortened URL: <a href="${shortenedUrl}">${shortenedUrl}</a>`;
+        // const div = document.createElement('div');
+        // div.className = 'shortened-url';
+        // div.innerHTML = `Here is your shortened URL: <a href="${shortenedUrl}">${shortenedUrl}</a>`;
 
-        content.appendChild(div);
+        // content.appendChild(div);
     } catch (error) {
         alert('Error: ' + error.message);
     }
-})
-
-btn.addEventListener('click', async (e) => {
-    e.preventDefault();
-    const url = originalURL.value;
 })
